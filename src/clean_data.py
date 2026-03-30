503cd855b4825c49a2a56db338f49fe20acba754
Ben Derieg <bderieg@deriegfamily.com>
2026-03-30 13:48:08 -0600

    updated data cleaning script

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
+from sklearn.impute import KNNImputer
 
 #################
 # set constants #
@@ -8,7 +9,8 @@ import numpy as np
 
 DATA_DIR = '../data/'
 PROCESSED_NAME = 'data_full.parquet'
-PROCESSED_CLEANED_NAME = 'data_full_cleaned.parquet'
+PROCESSED_CLEANED_NAME = 'data_full_cleaned_noimpute.parquet'
+PROCESSED_CLEANED_NAME_IMPUTED = 'data_full_cleaned_impute.parquet'
 FLOAT_COLS = [
             'T',
             'ra',
@@ -35,6 +37,7 @@ COLS_TO_DROP = [  # these columns are not physical, so they obviously will not r
             'i',
             'z'
         ]
+OUT_COLS = ['T', 'ug_color', 'ur_color', 'ui_color', 'uz_color', 'gr_color', 'gi_color', 'gz_color', 'ri_color', 'rz_color', 'iz_color', 'sb50_r', 'sb_conc_r']
 
 ###############
 # import data #
@@ -42,21 +45,24 @@ COLS_TO_DROP = [  # these columns are not physical, so they obviously will not r
 
 data = pd.read_parquet( os.path.join(DATA_DIR, PROCESSED_NAME) )
 
-#################################
-# perform some general cleaning #
-#################################
+##############################
+# cast some columns as float #
+##############################
 
-print("Cleaning some features...")
+print("Cleaning the data...")
 
-# drop any and all NaN
-data.dropna(inplace=True)
-
-# cast some columns as float
 for col in FLOAT_COLS:
     data[col] = pd.to_numeric(data[col], errors="coerce")
 
-# drop NaN again (could have been whitespace before)
-data.dropna(inplace=True)
+###################
+# data imputation #
+###################
+
+COLS_TO_IMPUTE = ['T', 'u', 'g', 'r', 'i', 'z', 'petroMag_r', 'petroR50_r', 'petroR90_r']
+
+data['imputed_brightness'] = data['z'].isna().astype(int)
+imputer = KNNImputer(n_neighbors=50, weights='uniform')
+data[COLS_TO_IMPUTE] = imputer.fit_transform(data[COLS_TO_IMPUTE])
 
 ########################
 # derive some features #
@@ -84,14 +90,40 @@ data['ri_color'] = data['r'] - data['i']
 data['rz_color'] = data['r'] - data['z']
 data['iz_color'] = data['i'] - data['z']
 
+# calculate type
+spel = np.array(['spiral']*len(data['T']))
+spel[ data['T'] < 0. ] = 'elliptical'
+data['morphology'] = spel
+
+###################
+# remove outliers #
+###################
+
+for cc in OUT_COLS:
+    nonan = np.array(data[cc])[~np.isnan(np.array(data[cc]))]
+    fq = np.quantile(nonan, 0.25)
+    tq = np.quantile(nonan, 0.75)
+    iqr = tq - fq
+    ubound = tq + 1.5*iqr
+    lbound = fq - 1.5*iqr
+    data = data[ data[cc]<ubound ]
+    data = data[ data[cc]>lbound ]
+
 #############################################
 # remove features that are no longer needed #
 #############################################
 
 data = data.drop(columns=COLS_TO_DROP)
 
+##############################
+# save cleaned imputed table #
+##############################
+
+data.to_parquet( os.path.join(DATA_DIR, PROCESSED_CLEANED_NAME_IMPUTED) )
+
 ######################
 # save cleaned table #
 ######################
 
+data = data[ data['imputed_brightness']==0 ]
 data.to_parquet( os.path.join(DATA_DIR, PROCESSED_CLEANED_NAME) )
